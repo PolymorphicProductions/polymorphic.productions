2a14557869d87b98b27b38b47ae6db4b56442718
Josh Chernoff <jchernoff@polymorphic.productions>
2018-11-02 12:26:30 -0700

    drop https for http

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :polymorphic_productions, PolymorphicProductionsWeb.Endpoint,
-  https: [
+  http: [
     :inet6,
     otp_app: :isafe,
     port: 443
