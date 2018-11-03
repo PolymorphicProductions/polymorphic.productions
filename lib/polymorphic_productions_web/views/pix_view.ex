b2f20fe27ab1f297e85f662c9d86bb5c717e77ff
Josh Chernoff <jchernoff@polymorphic.productions>
2018-11-03 09:17:17 -0700

    add twitter card

--- lib/polymorphic_productions_web/views/pix_view.ex
+++ lib/polymorphic_productions_web/views/pix_view.ex
@@ -4,4 +4,13 @@ defmodule PolymorphicProductionsWeb.PixView do
   def format_date(dt) do
     Timex.format!(dt, "{Mshort} {D}, {YYYY}")
   end
+
+  def render("social.show.html", assigns) do
+    {
+      :safe,
+      "<meta name=\"twitter:card\" content=\"summary_large_image\" />
+      <meta name=\"ttwitter:site\" content=\"@PolymorphicProd\" />
+      <meta property=\"og:image\" content=\"#{assigns[:pix].asset}\" />"
+    }
+  end
 end
