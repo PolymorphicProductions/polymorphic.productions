b3822353dfa8cc0e1c296fc7203312110d18b8d1
Josh Chernoff <jchernoff@polymorphic.productions>
2018-12-27 12:31:09 -0800

    fix test

--- test/polymorphic_productions_web/controllers/user_controller_test.exs
+++ test/polymorphic_productions_web/controllers/user_controller_test.exs
@@ -32,37 +32,32 @@ defmodule PolymorphicProductionsWeb.UserControllerTest do
       assert html_response(conn, 200) =~ "Please check the errors below."
     end
 
-    test "redirects guest to login when visiting User index", %{conn: conn} do
-      conn = get(conn, Routes.user_path(conn, :index))
-      assert redirected_to(conn) == Routes.session_path(conn, :new)
-    end
-
     test "redirects guest to login when visiting User show", %{conn: conn} do
-      conn = get(conn, Routes.user_path(conn, :show, 1))
+      conn = get(conn, Routes.user_path(conn, :show))
       assert redirected_to(conn) == Routes.session_path(conn, :new)
     end
 
     test "redirects guest to login when visiting User edit", %{conn: conn} do
-      conn = get(conn, Routes.user_path(conn, :edit, 1))
+      conn = get(conn, Routes.user_path(conn, :edit))
       assert redirected_to(conn) == Routes.session_path(conn, :new)
     end
 
     test "redirects guest to login when visiting User update", %{conn: conn} do
-      conn = put(conn, Routes.user_path(conn, :update, 1))
+      conn = put(conn, Routes.user_path(conn, :update))
       assert redirected_to(conn) == Routes.session_path(conn, :new)
     end
 
-    test "redirects guest to login when visiting User delete", %{conn: conn} do
-      conn = delete(conn, Routes.user_path(conn, :delete, 1))
-      assert redirected_to(conn) == Routes.session_path(conn, :new)
-    end
+    # test "redirects guest to login when visiting User delete", %{conn: conn} do
+    #   conn = delete(conn, Routes.user_path(conn, :delete, 1))
+    #   assert redirected_to(conn) == Routes.session_path(conn, :new)
+    # end
   end
 
   describe "authed user" do
     setup [:log_user_in, :create_rando_user]
 
     test "renders form for editing current user", %{conn: conn, user: user} do
-      conn = get(conn, Routes.user_path(conn, :edit, user))
+      conn = get(conn, Routes.user_path(conn, :edit))
       assert html_response(conn, 200) =~ "Edit User"
     end
 
