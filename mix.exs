9d8651e354f63410d15dfcc7b64ce35212f38ae6
Josh Chernoff <jchernoff@polymorphic.productions>
2018-12-30 16:11:21 -0800

    fix issue related to parroty/excoveralls/issues/169

--- mix.exs
+++ mix.exs
@@ -79,7 +79,7 @@ defmodule PolymorphicProductions.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "coveralls", "credo --all"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
