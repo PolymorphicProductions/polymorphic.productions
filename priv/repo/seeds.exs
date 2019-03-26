58f82a90a5151def23f582868c4fc72974fd7c47
Josh Chernoff <jchernoff@polymorphic.productions>
2019-03-25 21:42:37 -0700

    add basic seed build

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PolymorphicProductions.Accounts
+alias PolymorphicProductions.Accounts.User
+alias PolymorphicProductions.Repo
+
+User |> Repo.delete_all()
+
+[%{name: "Admin", email: "jchernoff@polymorphic.productions", password: "password"}]
+|> Enum.each(fn attr ->
+  attr
+  |> Accounts.create_user()
+  |> elem(1)
+  |> Accounts.confirm_user()
+  |> elem(1)
+  |> User.admin_changeset(true)
+  |> Repo.update()
+end)
+
+[%{name: "User", email: "user@polymorphic.productions", password: "password"}]
+|> Enum.each(fn attr ->
+  attr
+  |> Accounts.create_user()
+  |> elem(1)
+  |> Accounts.confirm_user()
+  |> elem(1)
+end)
